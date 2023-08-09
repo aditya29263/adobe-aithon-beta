972f0af76e17627566c54d954c5b61b9e608b72a
aditya29263 <141861474+aditya29263@users.noreply.github.com>
2023-08-09 23:29:51 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,2 +1,6 @@
+import numpy as np
 from predict import predict
-
+object_ideal = predict("images/", show=True)
+object_other = predict("images/", show=True)
+similarity_score = calculate_similarity(object_ideal,object_other)
+return similarity_score
